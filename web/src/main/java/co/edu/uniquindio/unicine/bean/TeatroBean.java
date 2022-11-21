52ee05cb416d2e1c405a3b7c4d84a647135b439c
KevinOFranco <kevin.orlando.franco.ballejo@gmail.com>
2022-11-21 00:28:41 -0500

    gestionar teatro funcionando

--- web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
@@ -25,6 +25,12 @@ public class TeatroBean implements Serializable {
 
     private String ruta = "../index.xhtml?faces-redirect=true";
 
+    @Getter @Setter
+    private List<Teatro> teatros;
+
+    @Getter @Setter
+    private List<Teatro> teatrosSeleccionados;
+
     @Autowired
     private AdminTeatroServicio adminTeatroServicio;
     @Autowired
@@ -33,23 +39,72 @@ public class TeatroBean implements Serializable {
     @Getter @Setter
     private List<Ciudad> ciudades;
 
+    private boolean editar;
+
     @PostConstruct
     public void init(){
         teatro = new Teatro();
+        editar = false;
         ciudades = adminTeatroServicio.listarCiudades();
+        teatros = adminTeatroServicio.listarTeatros();
+        teatrosSeleccionados = new ArrayList<>();
     }
 
-    public String registrarTeatro(){
+    public void registrarTeatro(){
         try {
-            teatro.setAdministradorTeatro(adminServicio.obtenerAdminTeatro(6));
-            adminTeatroServicio.crearTeatro(teatro);
-            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Teatro registrado");
-            FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
-            return ruta;
+            if (!editar){
+                teatro.setAdministradorTeatro(adminServicio.obtenerAdminTeatro(6));
+                Teatro registro = adminTeatroServicio.crearTeatro(teatro);
+                teatros.add(registro);
+                teatro = new Teatro();
+
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Teatro registrado");
+                FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
+            }
+            else {
+                adminTeatroServicio.actualizarTeatro(teatro);
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Teatro actualizado");
+                FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
+            }
         }catch (Exception e){
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
             FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
         }
-        return "";
+    }
+
+    public void eliminarTeatros (){
+        teatrosSeleccionados.forEach( teatroAux -> {
+            try {
+                adminTeatroServicio.eliminarTeatro(teatroAux.getId());
+                teatros.remove(teatroAux);
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Eliminación exitosa");
+                FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
+            } catch (Exception e) {
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
+                FacesContext.getCurrentInstance().addMessage("mensaje_bean", facesMessage);
+            }
+        });
+        teatrosSeleccionados.clear();
+    }
+
+    public String getMensajeEliminar(){
+        if (teatrosSeleccionados.isEmpty())
+            return "Eliminar";
+        else
+            return teatrosSeleccionados.size() == 1 ? "Eliminar 1 elemento" : "Eliminar " + teatrosSeleccionados.size() + " elementos";
+    }
+
+    public String getMensajeEditar(){
+        return editar ? "Editar teatro" : "Crear teatro";
+    }
+
+    public void seleccionarTeatro (Teatro teatroSeleccionado){
+        teatro = teatroSeleccionado;
+        editar = true;
+    }
+
+    public void crearTeatroDialog(){
+        editar = false;
+        teatro = new Teatro();
     }
 }
